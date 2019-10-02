739181396d11c5faebbbfb5f3583cc9c782641c0
Madhura Bhave <mbhave@pivotal.io>
2019-10-01 18:23:45 -0700

    Use WebMvcConfigurer

--- src/main/java/com/example/releasenotes/web/WebConfiguration.java
+++ src/main/java/com/example/releasenotes/web/WebConfiguration.java
@@ -3,9 +3,10 @@ package com.example.releasenotes.web;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfiguration extends WebMvcConfigurationSupport {
+public class WebConfiguration implements WebMvcConfigurer {
 
 	private final LoggerHandlerInterceptor interceptor;
 
